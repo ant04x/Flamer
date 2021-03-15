50bcff2a615d2e09c5c47d2a927afd7a28406dbc
Ant04X <50796747+ant04x@users.noreply.github.com>
2021-03-15 19:09:28 +0100

    Barra de navegación hecha.

--- src/main/kotlin/WelcomeStyles.kt
+++ src/main/kotlin/WelcomeStyles.kt
@@ -10,26 +10,4 @@ object WelcomeStyles : StyleSheet("WelcomeStyles", isStatic = true) {
             padding(0.px)
         }
     }
-
-    fun applyGlobalStyle() {
-        styled.injectGlobal(styles.toString())
-    }
-
-    val textContainer by css {
-        padding(5.px)
-
-        backgroundColor = rgb(8, 97, 22)
-        color = rgb(56, 246, 137)
-    }
-
-    val textInput by css {
-        margin(vertical = 5.px)
-
-        fontSize = 14.px
-    }
-
-    val menuButtonStyle by css {
-        marginLeft = (-12).px
-        marginRight = 16.px
-    }
 } 
--- src/main/kotlin/welcome.kt
+++ src/main/kotlin/welcome.kt
@@ -2,24 +2,39 @@ import kotlinx.css.*
 import kotlinx.html.InputType
 import kotlinx.html.js.onChangeFunction
 import org.w3c.dom.HTMLInputElement
-import react.RBuilder
-import react.RComponent
-import react.RProps
-import react.RState
 import styled.css
 import styled.styledDiv
 import styled.styledInput
 import com.ccfraser.muirwik.components.*
+import com.ccfraser.muirwik.components.button.MButtonSize
 import com.ccfraser.muirwik.components.button.mButton
+import com.ccfraser.muirwik.components.button.mFab
 import com.ccfraser.muirwik.components.button.mIconButton
-import com.ccfraser.muirwik.components.styles.ThemeOptions
-import com.ccfraser.muirwik.components.styles.createMuiTheme
+import com.ccfraser.muirwik.components.list.*
+import com.ccfraser.muirwik.components.styles.*
+import kotlinx.css.Color
+import kotlinx.css.properties.BoxShadow
+import kotlinx.css.properties.BoxShadows
+import kotlinx.html.js.onClickFunction
+import kotlinx.html.js.onKeyDownFunction
+import kotlinx.html.role
+import react.*
+import react.dom.div
+import react.dom.select
+import react.dom.span
+import react.dom.style
+import styled.cssifyDeclaration
+import kotlin.js.Console
 
 
 data class WelcomeState(val name: String) : RState
 
 @JsExport
 class Welcome(props: RProps) : RComponent<RProps, WelcomeState>(props) {
+
+    private var temporaryLeftOpen: Boolean = false
+    private var drawerWidth = 21
+    private var value1: Any = 0
 
     init {
         // state = WelcomeState(props.name)
@@ -32,7 +47,7 @@ class Welcome(props: RProps) : RComponent<RProps, WelcomeState>(props) {
         @Suppress("UnsafeCastFromDynamic")
         val themeOptions: ThemeOptions = js("({palette: { type: 'placeholder', primary: {main: 'placeholder'}}})")
         themeOptions.palette?.type = "light"
-        themeOptions.palette?.primary.main = Colors.BlueGrey.shade400.toString()
+        themeOptions.palette?.primary.main = Colors.Blue.accent400.toString()
 
         mThemeProvider(createMuiTheme(themeOptions)) {
 
@@ -45,7 +60,12 @@ class Welcome(props: RProps) : RComponent<RProps, WelcomeState>(props) {
 
                         mToolbar {
 
-                            mIconButton("menu", color = MColor.inherit) {
+                            css {
+                                color = Color.black
+                                backgroundColor = Color.white
+                            }
+
+                            mIconButton("menu", color = MColor.inherit, onClick = { setState { temporaryLeftOpen = true } }) {
 
                                 css {
                                     marginLeft = (-12).px
@@ -56,10 +76,68 @@ class Welcome(props: RProps) : RComponent<RProps, WelcomeState>(props) {
                             mTypography("Flamer", variant = MTypographyVariant.h6, color = MTypographyColor.inherit) {
                                 css { flexGrow = 1.0 }
                             }
-                            mIconButton ("account_circle", color = MColor.inherit)
+                            mIconButton ("search", color = MColor.inherit)
                         }
                     }
+                    mDrawer(temporaryLeftOpen, onClose = { setState { temporaryLeftOpen = false } }) {
+                        mailPlaceholder(false)
+                    }
+                    mBottomNavigation(value1, true, onChange = { onChange, indexValue -> setState { value1 = indexValue } }) {
+
+                        css {
+
+                            boxShadow.clear()
+                            boxShadow.plusAssign(BoxShadow(color = rgba(0, 0, 0, 12.0), inset = false, offsetX = 0.px, offsetY = 1.px, blurRadius = 10.px, spreadRadius = 0.px)) // 3
+                            boxShadow.plusAssign(BoxShadow(color = rgba(0, 0, 0, 14.0), inset = false , offsetX = 0.px, offsetY = 4.px, blurRadius = 5.px, spreadRadius = 0.px)) // 2
+                            boxShadow.plusAssign(BoxShadow(color = rgba(0, 0, 0, 20.0), inset = false , offsetX = 0.px, offsetY = 2.px, blurRadius = 4.px, spreadRadius = (-1).px)) // 1
+
+                            position = Position.fixed
+                            left = 0.px
+                            right = 0.px
+                            bottom = 0.px
+                        }
+
+                        mBottomNavigationAction("Recents", mIcon("restore", addAsChild = false))
+                        mFab("add", MColor.primary, size = MButtonSize.large) { css { marginTop = (-28).px } }
+                        mBottomNavigationAction("Nearby", mIcon("location_on", addAsChild = false))
+
+                    }
                 }
+            }
+        }
+    }
+
+    private fun RBuilder.mailPlaceholder(fullWidth: Boolean) {
+        themeContext.Consumer { theme ->
+            div {
+                attrs.role = "button"
+                attrs.onClickFunction = { setState { temporaryLeftOpen = false }}
+                attrs.onKeyDownFunction = { setState { temporaryLeftOpen = false }}
+            }
+            mList {
+                css {
+                    backgroundColor = Color(theme.palette.background.paper)
+                    width = if (fullWidth) LinearDimension.auto else drawerWidth.em
+//                style = js { width = if (fullWidth) "auto" else drawerWidth; backgroundColor = "white" }
+                }
+                mListItemWithAvatar(
+                    "/img/profile.jpg",
+                    "Anais Fernandez",
+                    "anaisfernandez@gmail.com",
+                    alignItems = MListItemAlignItems.flexStart
+                )
+                mListSubheader("Etiquetas", disableSticky = true)
+                mListItemWithIcon("lightbulb", "PSP", divider = false)
+                mListItemWithIcon("vpn_key", "ADA", divider = false)
+                mListItemWithIcon("desktop_windows", "PMDM", divider = false)
+                mListItemWithIcon("book", "SGE", divider = false)
+                mListItemWithIcon("web_asset", "DIN", divider = false)
+                mListItemWithIcon("location_city", "ING", divider = false)
+
+                mDivider()
+                mListSubheader("Acciones", disableSticky = true)
+                mListItemWithIcon("sell", "Crear Etiqueta", divider = false)
+                mListItemWithIcon("settings", "Ajustes", divider = false)
             }
         }
     }
