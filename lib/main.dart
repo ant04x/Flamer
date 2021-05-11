2eae630882bf65abf44a4a3cd36085eccdff47b6
ant04x <antonioizquierdoalvarez@outlook.com>
2021-05-11 02:32:41 +0200

    Principales errores de compatibilidad de login web resueltos.

--- lib/main.dart
+++ lib/main.dart
@@ -4,30 +4,54 @@ import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  // Create the initialization Future outside of `build`:
+  @override
+  _AppState createState() => _AppState();
+}
 
-  // This widget is the root of your application.
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flamer',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.deepOrange,
-      ),
-      home: SplashScreen()
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        /*if (snapshot.hasError) {
+          return SomethingWentWrong();
+        }*/
+
+        // Once complete, show your application
+        /*if (snapshot.connectionState == ConnectionState.done) {
+          return MyAwesomeApp();
+        }*/
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return MaterialApp(
+            title: 'Flamer',
+            theme: ThemeData(
+              // This is the theme of your application.
+              //
+              // Try running your application with "flutter run". You'll see the
+              // application has a blue toolbar. Then, without quitting the app, try
+              // changing the primarySwatch below to Colors.green and then invoke
+              // "hot reload" (press "r" in the console where you ran "flutter run",
+              // or simply save your changes to "hot reload" in a Flutter IDE).
+              // Notice that the counter didn't reset back to zero; the application
+              // is not restarted.
+              primarySwatch: Colors.deepOrange,
+            ),
+            home: SplashScreen()
+        );
+      },
     );
   }
 }
