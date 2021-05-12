fff32a1b2fd862431a47ae1022643d187b5ba2b0
Ant04X <50796747+ant04x@users.noreply.github.com>
2021-05-12 23:12:02 +0200

    Revertiendo soporte web.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flamer/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
+main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(App());
 }
