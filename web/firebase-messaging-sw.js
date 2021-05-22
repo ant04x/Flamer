117e9221c1a0e605935582dd54450e65652b2b30
ant04x <antonioizquierdoalvarez@outlook.com>
2021-05-22 18:26:24 +0200

    Notificaciones web en marcha.

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,1 +1,38 @@
-console.log("")
+importScripts("https://www.gstatic.com/firebasejs/8.6.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.6.1/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/8.6.1/firebase-analytics.js");
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAXHhbN1UukQ8krXPRbLcQlTe5sSy2D0H8",
+    authDomain: "flamer-89f12.firebaseapp.com",
+    databaseURL: "https://flamer-89f12-default-rtdb.firebaseio.com",
+    projectId: "flamer-89f12",
+    storageBucket: "flamer-89f12.appspot.com",
+    messagingSenderId: "505657173480",
+    appId: "1:505657173480:web:35ba35eab60fe3cd2db05d",
+    measurementId: "G-D5J4VP3BVQ"
+});
+
+const messaging = firebase.messaging();
+
+// Optional:
+/*
+messaging.onBackgroundMessage((message) => {
+  console.log("onBackgroundMessage", message);
+});
+*/
+
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = "AVISO";
+  const notificationOptions = {
+    icon: '/icons/Icon-512.png',
+    badge: '/images/demos/badge-128x128.png'
+  };
+
+  return self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+// firebase.analytics();
