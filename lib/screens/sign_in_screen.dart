dcc89a2c6990ee79a210a689323f1764717bdf91
Ant04X <50796747+ant04x@users.noreply.github.com>
2021-05-25 01:49:49 +0200

    Bugs de pantalla de acceso solventados y tema oscuro.

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,9 +1,11 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flamer/utils/authentication.dart';
 import 'package:flamer/utils/messaging.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:flutter/material.dart';
@@ -33,35 +35,43 @@ class _SignInScreenState extends State<SignInScreen> {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final textTheme = theme.textTheme;
+    ThemeData currentTheme = Theme.of(context);
+    Brightness currentBrightness = currentTheme.brightness;
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: currentBrightness == Brightness.light
+          ? Colors.white
+          : Colors.black,
       body: SafeArea(
         child: Center(
           child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+            padding: EdgeInsets.only(top: 16, bottom: 88, left: 32, right: 32),
             child: Column(
               mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SvgPicture.asset(
-                  "assets/todo-draw.svg",
-                  fit: BoxFit.scaleDown,
-                  placeholderBuilder: (context) => Container(height: 500, child: Center(child: CircularProgressIndicator(),),),
-                  width: 500,
+                Flexible(
+                  flex: 4,
+                  child: SvgPicture.asset(
+                    "assets/todo-draw.svg",
+                    fit: BoxFit.scaleDown,
+                    placeholderBuilder: (context) => Container(width: 400, child: Center(child: CircularProgressIndicator(),),),
+                    width: 400,
+                  ),
                 ),
-                Text(
-                  'Empieza a usar Flamer para que no se te olvide nada de la lista de la compra, del trabajo o de cualquier otra cosa.',
-                  style: textTheme.headline6!.copyWith(height: 1.5),
-                  textAlign: TextAlign.center,
-                ),
+                Flexible(
+                  child: AutoSizeText(
+                    'Empieza a usar Flamer para que no se te olvide nada de la lista de la compra, del trabajo o de cualquier otra cosa.',
+                    style: textTheme.headline6!.copyWith(height: 1.5),
+                    maxLines: 4,
+                    textAlign: TextAlign.center,
+                  ),
+                )
               ],
             ),
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
-        backgroundColor: Colors.pink.shade900,
-        foregroundColor: Colors.white,
         onPressed: () async {
           User? user = await Authentication.signInWithGoogle(context: context);
           // await Messaging.subscribeNotifications(user);
